479cb0b99ff01eb37e2238b2eca51529ab6d3324
gilangerlangga90 <42725308+gilangerlangga90@users.noreply.github.com>
2018-10-01 10:46:29 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,14 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Komentar</title>
+</head>
+<body>
+<h1 align="center"> FORM KOMENTAR </h1>
+<form method="post" action="proseskomentar.php">
+	Silahkan masukkan komentar anda : <br>
+	<textarea rows="10" cols="40" name="komen"></textarea>
+	<input type="submit" name="kirim" value="Kirim">
+</form>
+</body>
+</html>
